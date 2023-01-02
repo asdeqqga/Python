3f04c657b1f4cff34a24569d62a1391c9054d184
soungjun <jungy0309@naver.com>
2023-01-02 17:49:28 +0900

    2023/01/02

---
+++ Ch01/hello.py
@@ -0,0 +1,9 @@
+"""
+날짜 : 2023/01/02
+이름 : 라성준
+내용 : 파이썬 hello world 실습
+"""
+
+print('Hello world!')
+print("Hello Python!")
+print("안녕하세요. 파이썬!")
---
+++ Ch02/3.연산자.py
@@ -0,0 +1,52 @@
+"""
+날짜 : 2023/01/02
+이름 : 라성준
+내용 : 파이썬 연산자 실습
+"""
+
+# 대입연사자
+a = 1
+b = c = d = 0
+e, f, g = 7, True, 'Apple'
+
+print('a : ', a)
+print('c : ', c)
+print('f : ', f)
+print('g : ', g)
+
+# 산술연사자
+num1 = 1
+num2 = 2
+num3, num4 = 3, 4
+
+r1 = num1 + num2
+r2 = num1 - num2
+r3 = num2 * num3
+r4 = num2 ** num3
+r5 = num4 / num3
+r6 = num4 // num3
+r7 = num4 % num3
+
+print('r1 :', r1)
+print('r2 :', r2)
+print('r3 :', r3)
+print('r4 :', r4)
+print('r5 :', r5)
+print('r6 :', r6)
+print('r7 :', r7)
+
+# 복합대입연사자
+num5, num6, num7, num8 = 5, 6, 7, 8
+
+num5 += 1
+num6 -= 2
+num7 *= 3
+num8 /= 4
+
+print('num5 :', num5)
+print('num6 :', num6)
+print('num7 :', num7)
+print('num8 :', num8)
+
+# 비교연사자
+# 논리연사자
